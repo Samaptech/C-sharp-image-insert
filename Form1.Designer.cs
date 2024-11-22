c065800df9911d182cb3e55bd7b03c13fc601f8d
Samaptech <sameer2308b@aptechgdn.net>
2024-11-23 01:50:42 +0500

    C sharp image insert

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Insert_image_in_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.productImage = new System.Windows.Forms.PictureBox();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.imgBtn = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.Label();
+            this.pathName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.productImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // productImage
+            // 
+            this.productImage.Location = new System.Drawing.Point(476, 85);
+            this.productImage.Name = "productImage";
+            this.productImage.Size = new System.Drawing.Size(441, 426);
+            this.productImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.productImage.TabIndex = 0;
+            this.productImage.TabStop = false;
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Location = new System.Drawing.Point(186, 298);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(173, 88);
+            this.insertBtn.TabIndex = 1;
+            this.insertBtn.Text = "Insert Image";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // imgBtn
+            // 
+            this.imgBtn.Location = new System.Drawing.Point(186, 159);
+            this.imgBtn.Name = "imgBtn";
+            this.imgBtn.Size = new System.Drawing.Size(173, 88);
+            this.imgBtn.TabIndex = 1;
+            this.imgBtn.Text = "Select Image";
+            this.imgBtn.UseVisualStyleBackColor = true;
+            this.imgBtn.Click += new System.EventHandler(this.imgBtn_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.AutoSize = true;
+            this.fileName.Location = new System.Drawing.Point(72, 432);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(0, 20);
+            this.fileName.TabIndex = 2;
+            // 
+            // pathName
+            // 
+            this.pathName.AutoSize = true;
+            this.pathName.Location = new System.Drawing.Point(72, 475);
+            this.pathName.Name = "pathName";
+            this.pathName.Size = new System.Drawing.Size(0, 20);
+            this.pathName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 568);
+            this.Controls.Add(this.pathName);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.imgBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.productImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.productImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox productImage;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Button imgBtn;
+        private System.Windows.Forms.Label fileName;
+        private System.Windows.Forms.Label pathName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insert_image_in_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string imgName;
+        SqlConnection con = new SqlConnection("Data Source=COMPUTER;Initial Catalog=onlyImageUpload;Integrated Security=True");
+
+        private void imgBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.jpg; *.png; *.jpeg)|*.jpg; *.png; *.jpeg";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                productImage.Image = Image.FromFile(ofd.FileName);
+                pathName.Text = ofd.FileName;
+                fileName.Text = Path.GetFileName(ofd.FileName);
+                imgName = Path.GetFileName(ofd.FileName);
+
+                if (!Directory.Exists(Path.Combine(Application.StartupPath,"Product_Image")))
+                {
+                    Directory.CreateDirectory(Path.Combine(Application.StartupPath, "Product_Image"));
+                }
+                String destFileName = Path.Combine(Application.StartupPath, "Product_Image", imgName);
+                File.Copy(ofd.FileName, destFileName, true);
+            }
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO product VALUES(@img)", con);
+            cmd.Parameters.AddWithValue("@img" , imgName);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully Inserted");
+            con.Close();
+        }
+    }
+}
